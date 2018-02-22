0772ecae76195f0aff27fc5314f5e583a5087f5d
orazdow <ollierazdow@hotmail.com>
2018-02-22 18:58:38 -0500

    ok

--- main.cpp
+++ main.cpp
@@ -1,24 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/* 
- * File:   main.cpp
- * Author: ollie
- *
- * Created on February 21, 2018, 6:11 PM
- */
 
-#include <cstdlib>
+#include <string.h>
+#include <stdio.h>
 
 using namespace std;
 
-/*
- * 
- */
+
 int main(int argc, char** argv) {
+
+	for(int i = 0; i < argc; i++){
+		printf("argument %i : %s", i, argv[i]);
+	}
 
     return 0;
 }
