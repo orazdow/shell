e983d8f8acf2e235c0b5ed6581657ea837a9c846
orazdow <ollierazdow@hotmail.com>
2018-02-24 19:46:30 -0500

    ok!

--- main.cpp
+++ main.cpp
@@ -1,17 +1,55 @@
-
-
 #include <string.h>
 #include <stdio.h>
 
+#define LINE_LEN 1024
+#define ARG_LEN 50
+
 using namespace std;
+
+char* args[ARG_LEN];
+char line[LINE_LEN];
+
+int getCmd(char* line, char** args){
+  char* p;
+  int aidx = 0;
+  p = strtok(line," \0");
+  if(strlen(p) < 1){return 0;}
+  while(p != NULL){
+    args[aidx] = p;
+    aidx = ++aidx%ARG_LEN;
+    p = strtok(NULL, " \0");
+  }
+  return 1;
+}
 
 
 int main(int argc, char** argv) {
 
-	for(int i = 0; i < argc; i++){
-		printf("argument %i : %s", i, argv[i]);
+	// char line[] = "hi hi hooo randal!";
+	// getCmd(line,args);
+	// for(int i = 0; i < ARG_LEN; i++){
+	// 	printf("%s\n",args[i]);
+	// }
+
+	while(1){
+
+		printf("myshell>");
+
+		fgets(line, LINE_LEN-1,stdin);
+
+		if(getCmd(line,args) == 0){ 
+			continue; 
+		}
+
+		for(int i = 0; i < ARG_LEN; i++){
+			if(args[i])
+			printf("%s\n", args[i]);
+		}
+
+		memset(&line, 0, LINE_LEN);
+		memset(&args, 0, ARG_LEN);
+
 	}
 
     return 0;
 }
-
